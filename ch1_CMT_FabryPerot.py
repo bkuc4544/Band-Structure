92fdae591b66810c8b5a771684e9329662594389
Bharat Kuchhal <146112318+bkuc4544@users.noreply.github.com>
2025-03-14 17:03:26 -0400

    Update and rename ch1_cmt_fabryperot_sims.py to ch1_CMT_FabryPerot.py

--- ch1_CMT_FabryPerot.py
+++ ch1_CMT_FabryPerot.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Dec  6 20:37:46 2024
+@author: Bharat Kuchhal
 
-@author: bkuch
-
-Ch1, Eqs. (1.42)
+Description: The code uses coupled-mode theory to determine the transmission and reflection results for a Fabry-Perot cavity.
 """
 
 import numpy as np
@@ -13,30 +11,16 @@ import matplotlib.pyplot as plt
 Q1 = 1000
 Q2 = 1000
 
-domegas = np.linspace(-0.01, 0.01, 1001)    #domega = omega/omega0
-
-#%%
-# T = (4/tau1/tau2) / ((omega - omega0)^2 + (1/tau1 + 1/tau2)^2)
-
-# Let domega = omega/omega0 - 1
-# one_tau1 = 1/tau1 = w0/2Q_1, i.e., 1/tau1 = 1/2Q_1 normalized by w0
-# one_tau2 = 1/tau2 = w0/2Q_2, i.e., 1/tau2 = 1/2Q_2 normalized by w0
-
-# t = s2-/s1+ = -(2 * sqrt(one_tau1*one_tau2)) / (1j*domegas - one_tau1 - one_tau2)
-# Trans = abs(t) ** 2
- 
-# and
-# r = - (1 + (2*one_tau1)/(1j*domegas - one_tau1 - one_tau2))
-# Refl = abs(r) ** 2
-
-#%%
+domegas = np.linspace(-0.01, 0.01, 1001)
 
 one_tau1 = 1/2/Q1
 one_tau2 = 1/2/Q2
 
+# Transmissivity
 t = -(2*np.sqrt(one_tau1*one_tau2)) / (1j*domegas - one_tau1 - one_tau2)
 Trans = np.abs(t) ** 2    # Steady-state Transmission
 
+# Reflectivity
 r = -(1 + (2*one_tau1)/(1j*domegas - one_tau1 - one_tau2))
 Refl = np.abs(r) ** 2     # Steady-state Reflection
 
